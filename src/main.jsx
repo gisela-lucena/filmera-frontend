88b0838e0a1c4c6fa0d14897becc803f416383e1
Gisela Elia <lucena.gisela@gmail.com>
2026-05-01 17:59:09 -0400

    updates app.jsx, main.jsx and index.css and creates pages Index.jsx and notfound.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,5 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 import "./index.css";
-import App from "./components/App.jsx";
-import { HashRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </StrictMode>
-);
+createRoot(document.getElementById("root")).render(<App />);

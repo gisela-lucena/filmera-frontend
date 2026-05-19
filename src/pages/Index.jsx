58f98d099aac0eb1b72ae21925c3c16f6167db2f
Gisela Elia <lucena.gisela@gmail.com>
2026-05-18 23:21:47 -0400

    updates currentUser on Index.jsx

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -7,21 +7,20 @@ import SwipeDemo from "../components/SwipeDemo/SwipeDemo";
 import CTA from "../components/CTA/CTA";
 import Footer from "../components/Footer/Footer";
 
-const Index = () => (
-    <main className="page">
-        <Navbar
-            currentUser={currentUser}
-            onLogin={onLogin}
-            onLogout={onLogout}
-        />
-        <Hero />
-        <Problem />
-        <HowItWorks />
-        <Features />
-        <SwipeDemo />
-        <CTA />
-        <Footer />
-    </main>
+const Index = ({ currentUser, onLogin, onLogout }) => (<main className="page">
+    <Navbar
+        currentUser={currentUser}
+        onLogin={onLogin}
+        onLogout={onLogout}
+    />
+    <Hero />
+    <Problem />
+    <HowItWorks />
+    <Features />
+    <SwipeDemo />
+    <CTA />
+    <Footer />
+</main>
 
 );
 
